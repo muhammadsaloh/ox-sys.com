30a809360e7b9f7e9cd13e4c767d95ccfd8504d3
muhammadsaloh <muhammadsaloh1706@gmail.com>
2021-09-28 01:21:28 +0500

    App js connected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+const AuthenticatedApp = React.lazy(() =>
+	import("./authenticated-app/Authenticated-app")
+);
+
+const UnAuthenticatedApp = React.lazy(() =>
+	import("./unauthenticated-app/UnAuthenticated-app")
+);
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello APP</h1>
-    </div>
-  );
+	const user = window.localStorage.getItem("__token__");
+	return (
+		<>
+			<React.Suspense fallback="...Loading">
+				{user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
+			</React.Suspense>
+		</>
+	);
 }
 
 export default App;
